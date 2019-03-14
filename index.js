588c7841d32e67af5efd28f7fdc604de66695d23
Arijit Dasgupta <arijit.rivalslayer@gmail.com>
2019-03-14 13:36:37 +0530

    Initial working commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Test Log");
+const express = require('express');
+const process = require('process');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+
+const application = express();
+
+const filename = process.env['FILENAME'] ? `${process.env['FILENAME']}.log` : 'http-appender.log';
+const port = isNaN(parseInt(process.env['PORT'])) ? 8300 : parseInt(process.env['PORT']);
+
+const filepath = path.resolve('.', 'log_dump', filename);
+
+application.set('port', port);
+application.use(bodyParser.raw({ 
+  inflate: true,
+  limit: '100kb',
+  type: '*/*'
+}));
+
+application.post('/', (req, res) => {
+  fs.appendFile(filepath, req.body, () => {
+    res.send('OK');
+  });
+});
+
+application.listen(application.get('port'), () => {
+  console.info(`Listening on ${application.get('port')}`);
+});
